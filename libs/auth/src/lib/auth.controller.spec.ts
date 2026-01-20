2f645a068c1e00e6742a4009e4ba36f9525a8bfd
Pream Totaram <pream@preamtotaram.com>
2026-01-19 21:10:07 -0500

    Controller

--- libs/auth/src/lib/auth.controller.spec.ts
+++ libs/auth/src/lib/auth.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { register } from 'module';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -7,6 +9,14 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: {
+            login: jest.fn()
+          }
+        }
+      ]
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
--- libs/auth/src/lib/auth.controller.ts
+++ libs/auth/src/lib/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Post, Req, UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { GetUser } from './decorators/get-user.decorator';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) { }
+
+  @UseGuards(LocalAuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  async login(@GetUser() user: any) {
+    return this.authService.login(user);
+  }
+
+}
